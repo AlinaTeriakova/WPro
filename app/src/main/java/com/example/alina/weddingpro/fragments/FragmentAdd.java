a9958703c4a7ae3bc8791f1da07a2563262bf69f
AlinaTeriakova <AlinaTeriakova>
2018-05-04 15:04:35 +1000

    123

--- app/src/main/java/com/example/alina/weddingpro/fragments/FragmentAdd.java
+++ app/src/main/java/com/example/alina/weddingpro/fragments/FragmentAdd.java
@@ -3,7 +3,7 @@ package com.example.alina.weddingpro.fragments;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -75,7 +75,7 @@ public class FragmentAdd extends Fragment {
         }
     }
 
-    @Override
+   /* @Override
     public void onAttach(Context context) {
         super.onAttach(context);
         if (context instanceof OnFragmentInteractionListener) {
@@ -84,7 +84,7 @@ public class FragmentAdd extends Fragment {
             throw new RuntimeException(context.toString()
                     + " must implement OnFragmentInteractionListener");
         }
-    }
+    }*/
 
     @Override
     public void onDetach() {
--- app/src/main/java/com/example/alina/weddingpro/fragments/FragmentWeddings.java
+++ app/src/main/java/com/example/alina/weddingpro/fragments/FragmentWeddings.java
@@ -1,5 +1,6 @@
 package com.example.alina.weddingpro.fragments;
 
+import android.app.FragmentTransaction;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
@@ -19,7 +20,7 @@ import com.example.alina.weddingpro.R;
  * create an instance of this fragment.
  */
 public class FragmentWeddings extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
+    //FragmentAdd fadd;// TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
@@ -59,8 +60,15 @@ public class FragmentWeddings extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        //fadd = new FragmentAdd();
     }
 
+
+    /*public void clickButton(View veiw){
+
+        FragmentTransaction ftran = getFragmentManager().beginTransaction();
+        ftran.replace(R.id.container, fadd);
+    }*/
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
